90587f09e5de9a3d9ea0c446785f028bbd3e6026
chensong <chensongbj25@gmail.com>
2022-10-07 03:08:01 +0800

    [jpeg_ffmpeg_demo]

---
+++ jpeg_demo/jpeg_ffmpeg/main.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+#include <cstdio>
+#include <cstdlib>
+
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavutil/imgutils.h>
+#include <libavutil/error.h>
+#include <libavutil/frame.h>
+#include <libavutil/log.h>#include <libavcodec/codec.h>
+
+#include <libavformat/avformat.h>
+#include <libavdevice/avdevice.h>
+}
+ 
+ 
+ int main(int argc, char * argv[])
+ {
+	 
